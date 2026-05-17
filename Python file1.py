97ab59b611d57b01c616723beb2433f0414e4fdd
Pythonstart <dyrochok822@gmail.com>
2026-05-17 21:32:45 +0900

    Create Python file1.py

---
+++ Python file1.py
@@ -0,0 +1,13 @@
+import time
+
+print("Welcome to the mini tutorial!")
+print("Type an contunie")
+hehe = input("C://system32/:")
+
+if hehe == "contunie":
+	print("eehhhh...im tired :P, GL bro.")
+	print("💤💤💤💤💤💤")
+else:
+	print("No good end")
+	time.sleep(2)
+	print(">:( " * 100000)
